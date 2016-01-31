76e461e69897e39bdd353ab977ae171170f0d684
Alexar <hi@alexar.me>
2016-01-31 22:04:43 +1100

    v1.0

---
+++ rmsafe.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Usage $ rmsafe [whatever]
+#
+# TODO: support multiple files later
+
+target=$1
+backupdir=$HOME/.rmsafe
+mkdir -p $backupdir
+tmpname=$backupdir/$(date +%Y%m%d%H%M%S)-$(basename $target)
+mv $target $tmpname
+RMSAFE_LAST=$tmpname
+echo "Backup is kept at "$tmpname
+export RMSAFE_LAST
