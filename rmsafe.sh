83bc5448ecc59125ad29d0aedd8a59979d7668bf
Alexar <hi@alexar.me>
2016-02-04 05:12:57 +1100

    Fix bug: garbage collection fail because of read-only assets

--- rmsafe.sh
+++ rmsafe.sh
@@ -9,6 +9,8 @@ target=$1
 backupdir=$HOME/.rmsafe
 mkdir -p $backupdir
 tmpname=$backupdir/$(date +%Y%m%d%H%M%S)-$(basename $target)
+# read-only files would `mv` for now but would cause trouble later when we `rm` for gc.
+chmod -R u+w $target
 mv $target $tmpname
 RMSAFE_LAST=$tmpname
 echo "Backup is kept at "$tmpname
